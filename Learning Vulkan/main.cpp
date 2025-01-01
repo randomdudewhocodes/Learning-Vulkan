ebc7b182bec19319914dfaaf4237f6df97599ffa
randomdudewhocodes <nguyen07112007@gmail.com>
2025-01-01 22:08:02 +0700

    Creating an instance

--- Learning Vulkan/main.cpp
+++ Learning Vulkan/main.cpp
@@ -16,6 +16,7 @@ public:
     }
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow()
     {
@@ -27,15 +28,48 @@ private:
         window = glfwCreateWindow(960, 540, "Vulkan", nullptr, nullptr);
     }
 
-    void initVulkan() { }
+    void initVulkan()
+    {
+        // Create an instance
+        createInstance();
+    }
 
     void mainLoop() { while (!glfwWindowShouldClose(window)) glfwPollEvents(); }
 
     void cleanup()
     {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+            throw std::runtime_error("failed to create instance!");
     }
 };
 
